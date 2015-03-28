442491cfda4789c11b1e90d9d433393b56908cda
kamichidu <c.kamunagi@gmail.com>
2015-03-28 19:43:49 +0900

    Run for test.

---
+++ themis.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/sh
+
+if ! [ -d .deps ]; then
+    git clone https://github.com/thinca/vim-themis .deps/themis/
+fi
+
+./.deps/themis/bin/themis --runtimepath $HOME/.bundle/vim-prettyprint/ --recursive $*
