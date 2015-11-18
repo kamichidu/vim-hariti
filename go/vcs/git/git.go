f42ee6d86729fba7c0db6695195f93b76258a978
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2015-11-18 19:19:43 +0900

    simplify

--- go/vcs/git/git.go
+++ go/vcs/git/git.go
@@ -14,43 +14,24 @@ func NewGit() *Git {
 	return &Git{}
 }
 
-func (self *Git) Install(url string, path string) error {
-	log.Printf("Cloning %s to %s\n", url, path)
-
-	cmd := exec.Command("git", "clone", "--recursive", url, path)
+func run(name string, args ...string) error {
 	var stderr bytes.Buffer
+	cmd := exec.Command(name, args...)
 	cmd.Stderr = &stderr
 	err := cmd.Run()
 	if err != nil {
 		log.Printf("Got an error: %s\n---\n%s\n", err, stderr.String())
 		return fmt.Errorf("%s\n%v", stderr.String(), err)
 	}
-
 	return nil
+}
+
+func (self *Git) Install(url string, path string) error {
+	log.Printf("Cloning %s to %s\n", url, path)
+	return run("git", "clone", "--recursive", url, path)
 }
 
 func (self *Git) Update(_ string, path string) error {
 	log.Printf("Pulling in %s", path)
-
-	cmd := exec.Command("git", "pull", "--ff", "--ff-only")
-	// working directory
-	cmd.Dir = path
-	var stderr bytes.Buffer
-	cmd.Stderr = &stderr
-	err := cmd.Run()
-	if err != nil {
-		log.Printf("Got an error: %s\n---\n%s\n", err, stderr.String())
-		return fmt.Errorf("%s\n%v", stderr.String(), err)
-	}
-
-	cmd = exec.Command("git", "submodule", "update", "--init", "--recursive")
-	cmd.Dir = path
-	cmd.Stderr = &stderr
-	err = cmd.Run()
-	if err != nil {
-		log.Printf("Got an error: %s\n---\n%s\n", err, stderr.String())
-		return fmt.Errorf("%s\n%v", stderr.String(), err)
-	}
-
-	return nil
+	return run("git", "pull", "--ff", "--ff-only")
 }
